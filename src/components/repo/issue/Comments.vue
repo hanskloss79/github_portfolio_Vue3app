e5458e1f82b18c491f639f510e951b5f8d3bde0d
hanskloss79 <piohandz79@gmail.com>
2023-03-08 00:52:19 +0100

    Issues and Comments components created, octokit-rest.min.js added, octokitMixin created.

---
+++ src/components/repo/issue/Comments.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <div v-if="comments.length > 0">
+            <h4>Uwagi</h4>
+            <div v-for="c of comments" :key="c.id">
+                {{ c.user && c.user.login }} - {{ c.body }}
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+
+    props: {
+        owner: {
+            type: String,
+            required: true,
+        },
+        repo: {
+            type: String,
+            required: true,
+        },
+        issueNumber: {
+            type: Number,
+            required: true,
+        },
+    },
+
+    mixins: [octokitMixin],
+
+    data() {
+        return {
+            owner,
+            repo,
+            issue_number: issueNumber,
+        }
+    },
+
+    methods: {
+        async getIssueComments(owner, repo, issueNumber) {
+            if (
+                typeof owner !== "string" ||
+                typeof repo !== "string" ||
+                typeof issueNumber !== "number"
+            ) {
+                return;
+            }
+            const octokit = this.createOctokitClient();
+            const { data: comments } = await
+                octokit.issues.listComments({
+                    owner,
+                    repo,
+                    issue_number: issueNumber,
+                });
+            this.comments = comments;
+        }
+    },
+
+    watch: {
+        owner() {
+            this.getIssueComments();
+        },
+        repo() {
+            this.getIssueComments();
+        },
+        issueNumber() {
+            this.getIssueComments();
+        }
+
+    }
+}
+
+</script>
