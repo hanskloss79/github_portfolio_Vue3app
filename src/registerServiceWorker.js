2f0339581651c41efa8227d48554365b3e108a15
hanskloss79 <piohandz79@gmail.com>
2023-03-07 17:27:51 +0100

    GitHub App project became a PWA project

---
+++ src/registerServiceWorker.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+
+import { register } from 'register-service-worker'
+
+if (process.env.NODE_ENV === 'production') {
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    ready () {
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      )
+    },
+    registered () {
+      console.log('Service worker has been registered.')
+    },
+    cached () {
+      console.log('Zawartość została umieszczona w pamięci podręcznej do użytku w trybie offline.')
+    },
+    updatefound () {
+      console.log('Trwa pobieranie nowej zawartości.')
+    },
+    updated () {
+      console.log('Dostępna jest nowa zawartość. Odśwież stronę.')
+    },
+    offline () {
+      console.log('No internet connection found. App is running in offline mode.')
+    },
+    error (error) {
+      console.error('Error during service worker registration:', error)
+    }
+  })
+}
