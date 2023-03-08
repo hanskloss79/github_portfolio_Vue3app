ccab57072fe26bee4d0f9c97b325dd70ee9f44d3
hanskloss79 <piohandz79@gmail.com>
2023-03-08 12:08:35 +0100

    GitHubTokenForm and Repos components created.

---
+++ src/components/GitHubTokenForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <form @submit.prevent="saveToken">
+        <div>
+            <label for="githubToken">Token Githuba</label>
+            <br />
+            <input id="githubToken" v-model="githubToken" />
+        </div>
+        <div>
+            <input type="submit" value="Save token" />
+            <button type="button" v-on:click="clearToken">Wyczyść token</button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "GitHubTokenForm",
+    data() {
+        return {
+            githubToken: "",
+        };
+    },
+
+    beforeMount() {
+        //pobranie tokenu z lokalnego obiektu Storage gdzie dane są przechowywane pomiędzy sesjami 
+        this.githubToken = localStorage.getItem("github-token");
+    },
+
+    methods: {
+        saveToken() {
+            localStorage.setItem("github-token", this.githubToken);
+        },
+        clearToken() {
+            localStorage.clear();
+        }
+    },
+};
+
+
+</script>
